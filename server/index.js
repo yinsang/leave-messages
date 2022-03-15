8098d880d43c0e1517918216480110a22f381679
魏银鹏 <yinpeng.wyp@alibaba-inc.com>
2022-03-16 00:03:29 +0800

    feat: mongo 命令学习

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
 import http from 'http'
-import  redis from 'redis';
+import redis from 'redis';
 var client = redis.createClient('6379', '127.0.0.1')
 client.set('foo', 'bar', function () {
-    　　 // 此时SET命令执行完并返回结果，
-    　　 // 因为这里并不关心SET命令的结果，所以我们省略了回调函数的形参。
-    　　 client.get('foo', function (error, fooValue) {
-    　　　　　// error参数存储了命令执行时返回的错误信息，如果没有错误则返回null。
-    　　　　　// 回调函数的第二个参数存储的是命令执行的结果
-    　　　　　console.log(fooValue); // 'bar'
-    　　 });
+    // 此时SET命令执行完并返回结果，
+    // 因为这里并不关心SET命令的结果，所以我们省略了回调函数的形参。
+    client.get('foo', function (error, fooValue) {
+        // error参数存储了命令执行时返回的错误信息，如果没有错误则返回null。
+        // 回调函数的第二个参数存储的是命令执行的结果
+        console.log(fooValue); // 'bar'
     });
+});
 const server = http.createServer((req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*")
     const response = [
         {
             author: 'wyp',
-            content:'我爱中国'
+            content: '我爱中国'
         }
     ]
     res.end(JSON.stringify(response))
