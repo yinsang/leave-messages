ff487be5aa0b8663d20322dfe2d0e2a175f8cf9e
yinpeng.wyp <yinpeng.wyp@alibaba-inc.com>
2022-03-15 19:39:47 +0800

    feat: 添加database

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,15 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { getLeveMessageList } from "./api"
-
+import './App.css'
 export const App = () => {
+  const [list,setList] = useState(null)
   const getList = async () => {
     const list = await getLeveMessageList();
     console.log(list);
+    if(list){
+
+      setList(list)
+    }
 
   }
   useEffect(() => {
@@ -17,9 +22,18 @@ export const App = () => {
           nodejs留言板
         </h1>
       </header>
+      <textarea name="" id="" cols="30" rows="10"></textarea>
       <input type="button" value='发布留言' />
       <ul>
-
+{
+  list&&
+  list.map(({author, content})=>(
+    <li>
+      <div className="author">{author}</div>
+      <div className="content">{content}</div>
+    </li>
+  ))
+}
       </ul>
     </div>
   )
